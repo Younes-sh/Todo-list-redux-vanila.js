27753cab8c2a7a2fef90ebaf84059b68f224cab0
younes-sh <sheikhlaryounes@gmail.com>
2025-04-11 19:40:11 +0200

    I do coded in ADD_TODO

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
-const textInput = document.getElementById('#todo-input');
-const addButton = document.getElementById('#add-todo');
-const todoList = document.getElementById('#todo-list');
+const textInput = document.querySelector('#todo-input');
+const addButton = document.querySelector('#addTodo');
+const todoList = document.querySelector('#todo-list');
+const removBtn = document.querySelector('#remove-todo');
+
+// import { addTodo, completTodo, deleteTodo} from './Redux/actions.js';
+// import { addTodoCreator, completCreator, deleteTodoCreator} from './Redux/creators.js';
+
+const todoReducer = (state = [], action) => {
+    switch(action.type){
+        case "ADD_TODO": {
+            const newState = [...state];
+            const newTitle = action.title;
+            const newTodo = {
+                id: newState.length + 1,
+                title: newTitle,
+                isCompleted: false
+            }
+            newState.push(newTodo)
+            return newState;
+        }
+        case "COMPLET_TODO":{
+            
+        }
+        case "DELETE_TODO":{
+           
+        }
+        default:
+            return state;
+    }
+}
 
 
-todoList.innerText = `
-    <li>
-        <p>Todo1</p>
-        <button class="delete-todo">Delete</button>
-    </li>
-`
+const store = Redux.createStore(todoReducer);
+
+
+addButton.addEventListener('click', () => {
+    const titleTodo = textInput.value;
+    store.dispatch({type: "ADD_TODO", title: titleTodo});
+    textInput.value = '';
+    console.log(store.getState())
+});
+
+console.log(Redux)
